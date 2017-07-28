7abd55fe74139edf7025a9aba84ef78cccfc70e9
Daniel Fonseca Alves <dalraf@gmail.com>
2017-07-28 19:24:40 -0300

    Finalizado

--- polsys/models.py
+++ polsys/models.py
@@ -11,10 +11,6 @@ from django.db import models
 #   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
-from __future__ import unicode_literals
-
-from django.db import models
-
 
 class Alvo(models.Model):
     id_alvo = models.IntegerField(primary_key=True)

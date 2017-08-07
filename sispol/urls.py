fda0a0f61a4e2aa215795dfc621c9827764a6b16
Daniel Fonseca Alves <dalraf@gmail.com>
2017-08-07 11:30:06 -0300

    Removido completamente

--- sispol/urls.py
+++ sispol/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
